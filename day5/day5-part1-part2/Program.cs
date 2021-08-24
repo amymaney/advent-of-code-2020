828cbc8761b75bfef604b6c4a45c5e8d53cd28f8
Amy <amymaneyshaw@hotmail.com>
2021-08-24 14:22:27 +0100

    dd

---
+++ day5/day5-part1-part2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace day5_part1_part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
