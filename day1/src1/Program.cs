7a28dbe45d2c78cd8df31d8870c2d7ddfc5aa7c0
Amy <amymaneyshaw@hotmail.com>
2021-08-24 14:12:20 +0100

    a

---
+++ day1/src1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace src1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
