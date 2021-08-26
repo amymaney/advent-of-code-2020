fee0a84d8fd4e06668a44104b12b7a768157f327
Amy <amymaneyshaw@hotmail.com>
2021-08-26 12:58:58 +0100

    added notes

--- day8/day8-part1-part2/Program.cs
+++ day8/day8-part1-part2/Program.cs
@@ -4,9 +4,32 @@ namespace day8_part1_part2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
+
+// operation:
+// acc, jump, nop
+
+// argument:
+// signed number (e.g. +4 or -20)
+
+// acc:
+// increase/ decrease 'accumulator'
+// by whatever is in argument
+// accumulator starts at 0
+
+// jmp:
+// jumps to new instruction
+// depends on argument number
+// jmp +1 -> instruction immediately below
+// jmp -20 -> instruction 20 lines above next
+
+// nop:
+// does nothing
+// instruction immediately below executed next
+
+// find value of accuulator before the loop is executed again
